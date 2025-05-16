35db162fe6b070e29d9adb99a239831519e52dc4
Igor Comune <63658728+IgorComune@users.noreply.github.com>
2025-05-16 15:59:56 -0300

    16/05/2025

--- Material/2PythonML/Aula3/app.py
+++ Material/2PythonML/Aula3/app.py
@@ -1,107 +1,81 @@
-from flask import Flask, jsonify, request
-from flask_httpauth import HTTPBasicAuth
-from flasgger import Swagger
-from bs4 import BeautifulSoup
-import requests
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from config import Configs  # Import the Configs class
+from flask_jwt_extended import (JWTManager, create_access_token, jwt_required, get_jwt_identity)
 
 
 app = Flask(__name__)
+app.config.from_object(Configs)  # Use the Configs class
+app.config['JWT_VERIFY_SUB'] = False
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
-app.config['SWAGGER'] = {
-    'title': 'My Flask API',
-    'uiversion': 3
-}
 
-swagger = Swagger(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
-auth = HTTPBasicAuth()
 
-users = {
-    'user1': 'password1',
-    'user2': 'password2'
-}
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    time_minutes = db.Column(db.Integer, nullable=False)
 
-@auth.verify_password
-def verify_password(username, password):
-    if username in users and users[username] == password:
-        return username
-
-@app.route('/')
-def home():
-    return 'Hello, Flask'
-
-items = []
-
-@app.route('/hello', methods=['GET'])
-@auth.login_required
-def hello():
-    return jsonify({'message': 'Hello, World!'})
-
-@app.route('/items', methods=['GET'])  
-def get_items():
-    return jsonify(items)
-
-@app.route('/items', methods=['POST'])
-def create_item():
+@app.route('/register', methods=['POST'])
+def register_user():
     data = request.get_json()
-    items.append(data)
-    return jsonify(data), 201
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({'message': 'Usuário já existe'}), 400
+    new_user = User(username=data['username'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'Usuário registrado com sucesso'}), 201
 
-@app.route('/items/<int:item_id>', methods=['PUT'])
-def update_item(item_id):
+@app.route('/login', methods=['POST'])
+def login():
     data = request.get_json()
-    if 0 <= item_id < len(items):
-        items[item_id].update(data)
-        return jsonify(data)
-    return jsonify({'error': 'Item not found'}), 404        
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.password == data['password']:
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'access_token': access_token}), 200
+    return jsonify({'message': 'Credenciais inválidas'}), 401
 
-@app.route('/items/<int:item_id>', methods=['DELETE'])
-def delete_item(item_id):
-    if 0 <= item_id < len(items):
-        removed = items.pop(item_id)
-        return jsonify(removed)
-    return jsonify({'error': 'Item not found'}), 404        
+@app.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    return jsonify({'msg': f'Usuário com ID {current_user_id} acessou a rota protegida.'}), 200
 
-def get_title(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        title = soup.find('title')
-        return jsonify({'title': title.text})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 50
+@app.route('/recipes', methods=['GET'])
+@jwt_required()
+def create_recipe():
+    data = request.get_json()
+    new_recipe = Recipe(
+        title = data['title'],
+        ingredients = data['ingredients'],
+        time_minutes = data['time_minutes']
+        )
+    db.session.add(new_recipe)
+    db.session.commit()
+    return jsonify({'msg': 'Recipe created'}), 201
+
+@app.route('/recipes/<int:recipe_id>', methods=['PUT'])
+@jwt_required()
+def update_recipe(recipe_id):
+    data = request.get_json()
+    recipe = Recipe.query.get_or_404(recipe_id)
+    if 'title' in data:
+        recipe.title = data['title']
+    if 'ingredients' in data:
+        recipe.ingredients = data['ingredients']
+    if 'time_minutes' in data:
+        recipe.time_minutes = data['time_minutes']
         
-@app.route('/scrape/title', methods=['GET'])
-@auth.login_required
-def scrape_title():
-    url = request.args.get('url')
-    if not url:
-        return jsonify({'error': 'URL parameter is missing'}), 400
-    return get_title(url)
-
-def get_content(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        headers = []
-        for header_tag in ['h1', 'h2', 'h3']:
-            for header in soup.find_all(header_tag):
-                headers.append(header.get_text(strip=True))
-        paragraphs = [p.get_text(strip=True) for p in soup.find_all('p')]
-        return jsonify({'headers': headers, 'paragraphs': paragraphs})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/scrape/content', methods=['GET'])
-@auth.login_required
-def scrape_content():
-    url = request.args.get('url')
-    if not url:
-        return jsonify({'error': 'URL parameter is missing'}), 400
-    return get_content(url)
-    
+    db.session.commit()
+    return jsonify({'msg': 'Recipe updated'}), 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
